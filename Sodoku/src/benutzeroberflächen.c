93a4ee9e7d0b55deb86956fbc42d7fa8820f3038
ixexel <xelexpro@gmail.com>
2017-06-19 13:21:01 +0200

    Added comments

--- Sodoku/src/benutzeroberflächen.c
+++ Sodoku/src/benutzeroberflächen.c
@@ -2,38 +2,68 @@
 #include <stdio.h>
 #include <Windows.h>
 
-void BigWhiteSpace()
+/******************************************************************************
+Name: bigWhiteSpace
+Parameter: /
+Description:
+******************************************************************************/
+void bigWhiteSpace()
 {
   printf("                                                         ");
 }
 
+/******************************************************************************
+Name: whiteSpace
+Parameter: /
+Description:
+******************************************************************************/
 void whiteSpace()
 {
   printf("  ");
 }
 
+/******************************************************************************
+Name: quadWhiteSpace
+Parameter: /
+Description:
+******************************************************************************/
 void quadWhiteSpace()
 {
   printf("    ");
 }
 
+/******************************************************************************
+Name: lineBreaks
+Parameter: /
+Description:
+******************************************************************************/
 void lineBreaks()
 {
   printf("\n\n");
 }
 
+/******************************************************************************
+Name: printErrorMessage
+Parameter: /
+Description:
+******************************************************************************/
 void printErrorMessage(char fehler)
 {
   lineBreaks();
-  BigWhiteSpace();
+  bigWhiteSpace();
   whiteSpace();
   printf("Es ist ein Fehler aufgetreten: %c", fehler);
 }
 
+/******************************************************************************
+Name: showStartScreen
+Parameter: /
+Description:
+******************************************************************************/
 void showStartScreen()
 {
   lineBreaks();
-  BigWhiteSpace();
+  bigWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
@@ -41,13 +71,13 @@ void showStartScreen()
   whiteSpace();
   printf("Sudoku");
   lineBreaks();
-  BigWhiteSpace();
+  bigWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   printf("Schnelles Spiel");
   lineBreaks();
-  BigWhiteSpace();
+  bigWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
@@ -55,14 +85,14 @@ void showStartScreen()
   whiteSpace();
   printf(" Login");
   lineBreaks();
-  BigWhiteSpace();
+  bigWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   whiteSpace();
   printf("Registrieren");
   lineBreaks();
-  BigWhiteSpace();
+  bigWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
@@ -72,10 +102,15 @@ void showStartScreen()
   lineBreaks();
 }
 
+/******************************************************************************
+Name: printLogin
+Parameter: /
+Description:
+******************************************************************************/
 void printLogin()
 {
-   lineBreaks();
-  BigWhiteSpace();
+  lineBreaks();
+  bigWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
@@ -84,10 +119,15 @@ void printLogin()
   printf("Login");
 }
 
+/******************************************************************************
+Name: printRegistration
+Parameter: /
+Description:
+******************************************************************************/
 void printRegistration()
 {
-   lineBreaks();
-  BigWhiteSpace();
+  lineBreaks();
+  bigWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
@@ -95,56 +135,76 @@ void printRegistration()
   printf("Registrierung");
 }
 
+/******************************************************************************
+Name: printInputUsername
+Parameter: /
+Description:
+******************************************************************************/
 void printInputUsername()
 {
   lineBreaks();
-  BigWhiteSpace();
+  bigWhiteSpace();
   whiteSpace();
   printf("Bitte geben sie Ihren Username ein:");
 }
 
+/******************************************************************************
+Name: printInputPassword
+Parameter: /
+Description:
+******************************************************************************/
 void printInputPassword()
 {
   lineBreaks();
-  BigWhiteSpace();
+  bigWhiteSpace();
   whiteSpace();
   printf(" Bitte geben sie Ihr Passwort ein:");
 }
 
+/******************************************************************************
+Name: printInputPasswordRepeat
+Parameter: /
+Description:
+******************************************************************************/
 void printInputPasswordRepeat()
 {
   lineBreaks();
-  BigWhiteSpace();
+  bigWhiteSpace();
   whiteSpace();
   printf("Bitte wiederholen sie das Passwort:");
 }
 
+/******************************************************************************
+Name: showDifficulty
+Parameter: /
+Description:
+******************************************************************************/
 void showDifficulty()
 {
   lineBreaks();
-  BigWhiteSpace();
+  bigWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   whiteSpace();
   printf("Schwierigkeitsgrad");
-   lineBreaks();
-  BigWhiteSpace();
+  lineBreaks();
+  bigWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   whiteSpace();
   whiteSpace();
   printf("Leicht");
-   lineBreaks();
-  BigWhiteSpace();
+  lineBreaks();
+  bigWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   whiteSpace();
   whiteSpace();
   printf("Mittel");
-   lineBreaks();
-  BigWhiteSpace();
+  lineBreaks();
+  bigWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
@@ -153,10 +213,15 @@ void showDifficulty()
   printf("Schwer");
 }
 
+/******************************************************************************
+Name: loggedInStartScreen
+Parameter: /
+Description:
+******************************************************************************/
 void loggedInStartScreen()
 {
   lineBreaks();
-  BigWhiteSpace();
+  bigWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
@@ -164,7 +229,7 @@ void loggedInStartScreen()
   whiteSpace();
   printf("Sudoku");
   lineBreaks();
-  BigWhiteSpace();
+  bigWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
@@ -172,14 +237,14 @@ void loggedInStartScreen()
   whiteSpace();
   printf("Spielen");
   lineBreaks();
-  BigWhiteSpace();
+  bigWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   whiteSpace();
   printf("Bestenliste");
   lineBreaks();
-  BigWhiteSpace();
+  bigWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
@@ -187,7 +252,7 @@ void loggedInStartScreen()
   whiteSpace();
   printf("Regeln");
   lineBreaks();
-  BigWhiteSpace();
+  bigWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
@@ -195,7 +260,7 @@ void loggedInStartScreen()
   whiteSpace();
   printf("Logout");
   lineBreaks();
-  BigWhiteSpace();
+  bigWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
@@ -205,21 +270,36 @@ void loggedInStartScreen()
   lineBreaks();
 }
 
+/******************************************************************************
+Name: printFieldHorizontal
+Parameter: /
+Description:
+******************************************************************************/
 void printFieldHorizontal()
 {
-   printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4");
+  printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4"); // Zeigt ein Kreuz an und 6 horizontale Striche
 }
 
+/******************************************************************************
+Name: printFieldVertical
+Parameter: /
+Description:
+******************************************************************************/
 void printFieldVertical()
 {
-    printf("\xB3      ");
-    
+  printf("\xB3      "); // Zeigt einen vertikalen Strich an und 6 Leerzeichen
+
 }
 
+/******************************************************************************
+Name: printField
+Parameter: /
+Description:
+******************************************************************************/
 void printField()
 {
-  int i = 9;
-  int j = 9;
+  int i = 9; // Zählervariable
+  int j = 9; // Zählervariable
 
   lineBreaks();
 
@@ -227,31 +307,31 @@ void printField()
   {
     int x = 9;
     int y = 28;
-    BigWhiteSpace();
+    bigWhiteSpace();
     while (x > 0)
     {
       printFieldHorizontal();
       x--;
     }
     printf("\xC5\n");
-    BigWhiteSpace();
+    bigWhiteSpace();
     while (y > 0)
     {
       printFieldVertical();
 
-        if (y == 10||y == 20)
-        {
-          printf("\n");
-          BigWhiteSpace();
-        }
+      if (y == 10||y == 20)
+      {
+        printf("\n");
+        bigWhiteSpace();
+      }
 
-        y--;
+      y--;
 
     }
     printf("\xB3\n");
     i--;
   }
-  BigWhiteSpace();
+  bigWhiteSpace();
   while (j > 0)
   {
     printFieldHorizontal();
@@ -260,6 +340,11 @@ void printField()
   printf("\xC5\n");
 }
 
+/******************************************************************************
+Name: main
+Parameter: /
+Description:
+******************************************************************************/
 int main()
 {
   printField();
