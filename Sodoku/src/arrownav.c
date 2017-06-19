5491d6d551e4e83794f0ce1994404e1b4167ff94
NickSchikoraLumesse <nick.schikora@lumesse.com>
2017-06-19 12:12:09 +0200

    Navigation via ArrowKeys
    Overrides the chararray(keypressed) with up, down, left, right, return, escape, enter in capital letters.

---
+++ Sodoku/src/arrownav.c
@@ -0,0 +1,33 @@
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void navigation(char keypressed[])
+{
+	int ch;
+
+	while (1)
+	{
+		if (kbhit())
+		{
+			ch=getch();
+			if (ch==224 || ch==0)
+			{
+				ch=getch();
+			}
+			switch (ch)
+			{
+			case 72: keypressed="UP"; break;
+			case 80: keypressed="DOWN"; break;
+			case 75: keypressed="LEFT"; break;
+			case 77: keypressed="RIGHT"; break;
+			case 8: keypressed="RETURN"; break;
+			case 27: keypressed="ESCAPE"; break;
+			case 13: keypressed="ENTER"; break;
+			default:
+				break;
+			}
+		}
+	}
+}
