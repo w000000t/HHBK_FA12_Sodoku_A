b66d379bba8d21f824e545fc9bcf6d76578db466
w000000t <code@simon-marx.de>
2017-06-20 22:40:04 +0200

    Some minor bugfixes and improvements

--- Sodoku/src/sudoku.h
+++ Sodoku/src/sudoku.h
@@ -5,15 +5,16 @@
 #include "sqlite3.h"
 
 #define DB_FILE "sodoku_gruppe_a.db"
-#define SUDOKU_FIELDS_X_AXIS 8
-#define SUDOKU_FIELDS_Y_AXIS 8
+#define SUDOKU_FIELDS_X_AXIS 9
+#define SUDOKU_FIELDS_Y_AXIS 9
 
 typedef struct {
-	int disabled;
-	int value;
+	int disabled; // can the field be edited by the user
+	int value; // the value of the sudoku field
 }sudoku_field;
 
-int initSudoku(int difficulty, int sudokuFields[SUDOKU_FIELDS_X_AXIS][SUDOKU_FIELDS_Y_AXIS]);
+// prototypes
+int initSudoku(int difficulty, sudoku_field sudokuFields[SUDOKU_FIELDS_X_AXIS][SUDOKU_FIELDS_Y_AXIS]);
 int validateSudoku();
 int validateRow();
 int validateField();
