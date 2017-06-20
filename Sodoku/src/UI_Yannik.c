3e15cc3aa8a28bbdfe35a39b940f70cfe389fa7a
ixexel <xelexpro@gmail.com>
2017-06-20 13:13:15 +0200

    Add files via upload

---
+++ Sodoku/src/UI_Yannik.c
@@ -0,0 +1,413 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <Windows.h>
+#include "UI_Yannik.h"
+
+/******************************************************************************
+Name: bigWhiteSpace
+Parameter: /
+Description: Does nine Tabstops
+******************************************************************************/
+void bigWhiteSpace()
+{
+	printf("\t\t\t\t\t\t\t\t\t");// 9 Tabstops
+}
+
+/******************************************************************************
+Name: whiteSpace
+Parameter: /
+Description:Does one Tabstop
+******************************************************************************/
+void whiteSpace()
+{
+	printf("\t");
+}
+
+/******************************************************************************
+Name: quadWhiteSpace
+Parameter: /
+Description:Does four Tabstops
+******************************************************************************/
+void quadWhiteSpace()
+{
+	printf("\t\t\t\t");
+}
+
+/******************************************************************************
+Name: quadWhiteSpace
+Parameter: /
+Description:Does six Tabstops
+******************************************************************************/
+void hexaWhiteSpace()
+{
+	quadWhiteSpace();
+	whiteSpace();
+	whiteSpace();
+}
+
+/******************************************************************************
+Name: lineBreaks
+Parameter: /
+Description:
+******************************************************************************/
+void lineBreaks()
+{
+	printf("\n\n");
+}
+
+/******************************************************************************
+Name: printErrorMessage
+Parameter: /
+Description:
+******************************************************************************/
+void printErrorMessage(char error)
+{
+	lineBreaks();
+	bigWhiteSpace();
+	whiteSpace();
+	printf("Es ist ein Fehler aufgetreten: %c", error);
+}
+
+/******************************************************************************
+Name: showStartScreen
+Parameter: /
+Description:
+******************************************************************************/
+void showStartScreen()
+{
+	system("cls");
+	lineBreaks();
+	bigWhiteSpace();
+	printf("	Sudoku");
+	lineBreaks();
+	bigWhiteSpace();
+	printf("Schnelles Spiel");
+	lineBreaks();
+	bigWhiteSpace();
+	printf(" Login");
+	lineBreaks();
+	bigWhiteSpace();
+	printf("Registrieren");
+	lineBreaks();
+	bigWhiteSpace();
+	printf("	Beenden");
+	lineBreaks();
+}
+
+/******************************************************************************
+Name: printLogin
+Parameter: /
+Description:
+******************************************************************************/
+void printLogin()
+{
+	system("cls");
+	lineBreaks();
+	bigWhiteSpace();
+	printf("Login");
+}
+
+/******************************************************************************
+Name: printRegistration
+Parameter: /
+Description:
+******************************************************************************/
+void printRegistration()
+{
+	system("cls");
+	lineBreaks();
+	bigWhiteSpace();
+	printf("Registrierung");
+}
+
+/******************************************************************************
+Name: printInputUsername
+Parameter: /
+Description:
+******************************************************************************/
+void printInputUsername()
+{
+	lineBreaks();
+	bigWhiteSpace();
+	printf("Bitte geben sie Ihren Username ein:");
+}
+
+/******************************************************************************
+Name: printInputPassword
+Parameter: /
+Description:
+******************************************************************************/
+void printInputPassword()
+{
+	lineBreaks();
+	bigWhiteSpace();
+	printf(" Bitte geben sie Ihr Passwort ein:");
+}
+
+/******************************************************************************
+Name: printInputPasswordRepeat
+Parameter: /
+Description:
+******************************************************************************/
+void printInputPasswordRepeat()
+{
+	lineBreaks();
+	bigWhiteSpace();
+	printf("Bitte wiederholen sie das Passwort:");
+}
+
+void printSuccessMessage(char username)
+{
+	lineBreaks();
+	bigWhiteSpace();
+	whiteSpace();
+	printf("Es ist ein Fehler aufgetreten: %c", username);
+}
+
+/******************************************************************************
+Name: showDifficulty
+Parameter: /
+Description:
+******************************************************************************/
+void showDifficulty()
+{
+	system("cls");
+	lineBreaks();
+	quadWhiteSpace();
+	quadWhiteSpace();
+	printf("    Schwierigkeitsgrad");
+	lineBreaks();
+	bigWhiteSpace();
+	printf("  Leicht");
+	lineBreaks();
+	bigWhiteSpace();
+	printf("  Mittel");
+	lineBreaks();
+	bigWhiteSpace();
+	printf("  Schwer");
+}
+
+/******************************************************************************
+Name: loggedInStartScreen
+Parameter: /
+Description:
+******************************************************************************/
+void showLoggedInStartScreen()
+{
+	system("cls");
+	lineBreaks();
+	bigWhiteSpace();
+	printf("  Sudoku");
+	lineBreaks();
+	bigWhiteSpace();
+	printf("  Spielen");
+	lineBreaks();
+	bigWhiteSpace();
+	printf("Bestenliste");
+	lineBreaks();
+	bigWhiteSpace();
+	printf("  Regeln");
+	lineBreaks();
+	bigWhiteSpace();
+	printf("  Logout");
+	lineBreaks();
+	bigWhiteSpace();
+	printf("  Beenden");
+	lineBreaks();
+}
+
+/******************************************************************************
+Name: printFieldHorizontal
+Parameter: /
+Description:
+******************************************************************************/
+void printFieldHorizontal()
+{
+	printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4");
+	printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4");
+	printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4");
+	printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4");
+	printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4");
+	printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4");
+	printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4");
+	printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4");
+	printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4\xC5\n");
+}
+
+/******************************************************************************
+Name: printField
+Parameter: /
+Description:
+******************************************************************************/
+void printField(int * sudokuWerte[8][8])
+{
+
+	system("cls");
+
+	lineBreaks();
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("\xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   ", sudokuWerte[0][0], sudokuWerte[0][1], sudokuWerte[0][2], sudokuWerte[0][3], sudokuWerte[0][4], sudokuWerte[0][5]);
+	printf("\xB3  %d   \xB3  %d   \xB3  %d   \xB3\n", sudokuWerte[0][6], sudokuWerte[0][7], sudokuWerte[0][8]);
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3      \n");
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("\xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   ", sudokuWerte[1][0], sudokuWerte[1][1], sudokuWerte[1][2], sudokuWerte[1][3], sudokuWerte[1][4], sudokuWerte[1][5]);
+	printf("\xB3  %d   \xB3  %d   \xB3  %d   \xB3\n", sudokuWerte[1][6], sudokuWerte[1][7], sudokuWerte[1][8]);
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3      \n");
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("\xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   ", sudokuWerte[2][0], sudokuWerte[2][1], sudokuWerte[2][2], sudokuWerte[2][3], sudokuWerte[2][4], sudokuWerte[2][5]);
+	printf("\xB3  %d   \xB3  %d   \xB3  %d   \xB3\n", sudokuWerte[2][6], sudokuWerte[2][7], sudokuWerte[2][8]);
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3      \n");
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("\xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   ", sudokuWerte[3][0], sudokuWerte[3][1], sudokuWerte[3][2], sudokuWerte[3][3], sudokuWerte[3][4], sudokuWerte[3][5]);
+	printf("\xB3  %d   \xB3  %d   \xB3  %d   \xB3\n", sudokuWerte[3][6], sudokuWerte[3][7], sudokuWerte[3][8]);
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3      \n");
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("\xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   ", sudokuWerte[4][0], sudokuWerte[4][1], sudokuWerte[4][2], sudokuWerte[4][3], sudokuWerte[4][4], sudokuWerte[4][5]);
+	printf("\xB3  %d   \xB3  %d   \xB3  %d   \xB3\n", sudokuWerte[4][6], sudokuWerte[4][7], sudokuWerte[4][8]);
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3      \n");
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("\xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   ", sudokuWerte[5][0], sudokuWerte[5][1], sudokuWerte[5][2], sudokuWerte[5][3], sudokuWerte[5][4], sudokuWerte[5][5]);
+	printf("\xB3  %d   \xB3  %d   \xB3  %d   \xB3\n", sudokuWerte[5][6], sudokuWerte[5][7], sudokuWerte[5][8]);
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3      \n");
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("\xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   ", sudokuWerte[6][0], sudokuWerte[6][1], sudokuWerte[6][2], sudokuWerte[6][3], sudokuWerte[6][4], sudokuWerte[6][5]);
+	printf("\xB3  %d   \xB3  %d   \xB3  %d   \xB3\n", sudokuWerte[6][6], sudokuWerte[6][7], sudokuWerte[6][8]);
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3      \n");
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("\xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   ", sudokuWerte[7][0], sudokuWerte[7][1], sudokuWerte[7][2], sudokuWerte[7][3], sudokuWerte[7][4], sudokuWerte[7][5]);
+	printf("\xB3  %d   \xB3  %d   \xB3  %d   \xB3\n", sudokuWerte[7][6], sudokuWerte[7][7], sudokuWerte[7][8]);
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3      \n");
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("\xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   \xB3  %d   ", sudokuWerte[8][0], sudokuWerte[8][1], sudokuWerte[8][2], sudokuWerte[8][3], sudokuWerte[8][4], sudokuWerte[8][5]);
+	printf("\xB3  %d   \xB3  %d   \xB3  %d   \xB3\n", sudokuWerte[8][6], sudokuWerte[8][7], sudokuWerte[8][8]);
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3      \n");
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("Hilfe");
+	hexaWhiteSpace();
+	whiteSpace();
+	printf("  Beenden\n");
+	hexaWhiteSpace();
+	printf("F1");
+	hexaWhiteSpace();
+	whiteSpace();
+	printf("  ESC\n");
+
+
+
+	// int i = 9; // Zählervariable
+	// int j = 9; // Zählervariable
+
+	// lineBreaks();
+
+	// while (i > 0)
+	// {
+	//   int x = 9;
+	//   int y = 28;
+	//   bigWhiteSpace();
+	//   while (x > 0)
+	//   {
+	//     printFieldHorizontal();
+	//     x--;
+	//   }
+	//   printf("\xC5\n");
+	//   bigWhiteSpace();
+	//   while (y > 0)
+	//   {
+	//     printFieldVertical();
+
+	//     if (y == 10||y == 20)
+	//     {
+	//       printf("\n");
+	//       bigWhiteSpace();
+	//     }
+
+	//     y--;
+
+	//   }
+	//   printf("\xB3\n");
+	//   i--;
+	// }
+	//bigWhiteSpace();
+	// while (j > 0)
+	// {
+	//   printFieldHorizontal();
+	//   j--;
+	// }
+	// printf("\xC5\n");
+}
+
+void showHighscore()
+{
+	system("cls");
+	lineBreaks();
+	bigWhiteSpace();
+	printf("Bestenliste\n");
+}
+
+void showIntro()
+{
+	//printf(" .----------------.  .----------------.  .----------------.  .--");
+	//printf("--------------.  .----------------.  .----------------. \n");
+	//printf("| .--------------. || .--------------. || .--------------. || .-");
+	//printf("-------------. || .--------------. || .--------------. |\n");
+	//printf("| |    _______   | || | _____  _____ | || |  ________    | || | ");
+	//printf("____     | || |  ___  ____   | || | _____  _____ | |\n");
+	//printf("| |   /  ___  |  | || ||_   _||_   _|| || | |_   ___ `.  | || | ");
+	//printf(".'    `.   | || | |_  ||_  _|  | || ||_   _||_   _|| |\n");
+	//printf("| |  |  (__ \_|  | || |  | |    | |  | || |   | |   `. \ | || | ");
+	//printf("/  .--.  \  | || |   | |_/ /    | || |  | |    | |  | |\n");
+	//printf("| |   '.___`-.   | || |  | '    ' |  | || |   | |    | | | || | ");
+	//printf("| |    | |  | || |   |  __'.    | || |  | '    ' |  | |");
+}
+/******************************************************************************
+Name: main
+Parameter: /
+Description:
+******************************************************************************/
+int main()
+{
+	printField();
+
+
+	system("pause");
+	return 0;
+}
+
