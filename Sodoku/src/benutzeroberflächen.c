be0675d46b9c907b6e619d64563a4e345b41cb47
ixexel <xelexpro@gmail.com>
2017-06-20 10:52:59 +0200

    Bug fixing

--- Sodoku/src/benutzeroberflächen.c
+++ Sodoku/src/benutzeroberflächen.c
@@ -9,7 +9,7 @@ Description:
 ******************************************************************************/
 void bigWhiteSpace()
 {
-  printf("                                                         ");
+  printf("\t\t\t\t\t\t\t\t\t");// 9 Tabstops
 }
 
 /******************************************************************************
@@ -19,7 +19,7 @@ Description:
 ******************************************************************************/
 void whiteSpace()
 {
-  printf("  ");
+  printf("\t");
 }
 
 /******************************************************************************
@@ -29,7 +29,14 @@ Description:
 ******************************************************************************/
 void quadWhiteSpace()
 {
-  printf("    ");
+  printf("\t\t\t\t");
+}
+
+void hexaWhiteSpace()
+{
+	quadWhiteSpace();
+	whiteSpace();
+	whiteSpace();
 }
 
 /******************************************************************************
@@ -64,41 +71,19 @@ void showStartScreen()
 {
   lineBreaks();
   bigWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  whiteSpace();
-  whiteSpace();
-  printf("Sudoku");
+  printf("	Sudoku");
   lineBreaks();
   bigWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
   printf("Schnelles Spiel");
   lineBreaks();
   bigWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  whiteSpace();
-  whiteSpace();
   printf(" Login");
   lineBreaks();
   bigWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  whiteSpace();
   printf("Registrieren");
   lineBreaks();
   bigWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  whiteSpace();
-  whiteSpace();
-  printf("Beenden");
+  printf("	Beenden");
   lineBreaks();
 }
 
@@ -111,11 +96,6 @@ void printLogin()
 {
   lineBreaks();
   bigWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  whiteSpace();
-  whiteSpace();
   printf("Login");
 }
 
@@ -128,10 +108,6 @@ void printRegistration()
 {
   lineBreaks();
   bigWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  whiteSpace();
   printf("Registrierung");
 }
 
@@ -144,7 +120,6 @@ void printInputUsername()
 {
   lineBreaks();
   bigWhiteSpace();
-  whiteSpace();
   printf("Bitte geben sie Ihren Username ein:");
 }
 
@@ -157,7 +132,6 @@ void printInputPassword()
 {
   lineBreaks();
   bigWhiteSpace();
-  whiteSpace();
   printf(" Bitte geben sie Ihr Passwort ein:");
 }
 
@@ -170,8 +144,12 @@ void printInputPasswordRepeat()
 {
   lineBreaks();
   bigWhiteSpace();
-  whiteSpace();
   printf("Bitte wiederholen sie das Passwort:");
+}
+
+void printSuccessMessage()
+{
+
 }
 
 /******************************************************************************
@@ -182,35 +160,18 @@ Description:
 void showDifficulty()
 {
   lineBreaks();
-  bigWhiteSpace();
   quadWhiteSpace();
   quadWhiteSpace();
-  whiteSpace();
-  printf("Schwierigkeitsgrad");
+  printf("    Schwierigkeitsgrad");
   lineBreaks();
   bigWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  whiteSpace();
-  whiteSpace();
-  printf("Leicht");
+  printf("  Leicht");
   lineBreaks();
   bigWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  whiteSpace();
-  whiteSpace();
-  printf("Mittel");
+  printf("  Mittel");
   lineBreaks();
   bigWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  whiteSpace();
-  whiteSpace();
-  printf("Schwer");
+  printf("  Schwer");
 }
 
 /******************************************************************************
@@ -222,51 +183,22 @@ void loggedInStartScreen()
 {
   lineBreaks();
   bigWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  whiteSpace();
-  whiteSpace();
-  printf("Sudoku");
+  printf("  Sudoku");
   lineBreaks();
   bigWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  whiteSpace();
-  whiteSpace();
-  printf("Spielen");
+  printf("  Spielen");
   lineBreaks();
   bigWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  whiteSpace();
   printf("Bestenliste");
   lineBreaks();
   bigWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  whiteSpace();
-  whiteSpace();
-  printf("Regeln");
+  printf("  Regeln");
   lineBreaks();
   bigWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  whiteSpace();
-  whiteSpace();
-  printf("Logout");
+  printf("  Logout");
   lineBreaks();
   bigWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  quadWhiteSpace();
-  whiteSpace();
-  whiteSpace();
-  printf("Beenden");
+  printf("  Beenden");
   lineBreaks();
 }
 
@@ -277,7 +209,15 @@ Description:
 ******************************************************************************/
 void printFieldHorizontal()
 {
-  printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4"); // Zeigt ein Kreuz an und 6 horizontale Striche
+  printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4");
+	printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4");
+	printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4");
+	printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4");
+	printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4");
+	printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4");
+	printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4");
+	printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4");
+	printf("\xC5\xC4\xC4\xC4\xC4\xC4\xC4\xC5\n");
 }
 
 /******************************************************************************
@@ -298,48 +238,142 @@ Description:
 ******************************************************************************/
 void printField()
 {
-  int i = 9; // Zählervariable
-  int j = 9; // Zählervariable
+	lineBreaks();
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3\n");
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3\n");
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3\n");
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3\n");
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3\n");
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3\n");
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3\n");
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3\n");
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3\n");
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3\n");
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3\n");
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3\n");
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3\n");
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3\n");
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3\n");
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3\n");
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3\n");
+	hexaWhiteSpace();
+	printf("\xB3      \xB3      \xB3      \xB3      \xB3      \xB3      ");
+	printf("\xB3      \xB3      \xB3      \xB3\n");
+	hexaWhiteSpace();
+	printFieldHorizontal();
+	hexaWhiteSpace();
+	printf("Hilfe");
+	hexaWhiteSpace();
+	whiteSpace();
+	printf("  Beenden\n");
+	hexaWhiteSpace();
+	printf("F1");
+	hexaWhiteSpace();
+	whiteSpace();
+	printf("  ESC\n");
 
-  lineBreaks();
 
-  while (i > 0)
-  {
-    int x = 9;
-    int y = 28;
-    bigWhiteSpace();
-    while (x > 0)
-    {
-      printFieldHorizontal();
-      x--;
-    }
-    printf("\xC5\n");
-    bigWhiteSpace();
-    while (y > 0)
-    {
-      printFieldVertical();
 
-      if (y == 10||y == 20)
-      {
-        printf("\n");
-        bigWhiteSpace();
-      }
+ // int i = 9; // Zählervariable
+ // int j = 9; // Zählervariable
 
-      y--;
+ // lineBreaks();
 
-    }
-    printf("\xB3\n");
-    i--;
-  }
-  bigWhiteSpace();
-  while (j > 0)
-  {
-    printFieldHorizontal();
-    j--;
-  }
-  printf("\xC5\n");
+ // while (i > 0)
+ // {
+ //   int x = 9;
+ //   int y = 28;
+ //   bigWhiteSpace();
+ //   while (x > 0)
+ //   {
+ //     printFieldHorizontal();
+ //     x--;
+ //   }
+ //   printf("\xC5\n");
+ //   bigWhiteSpace();
+ //   while (y > 0)
+ //   {
+ //     printFieldVertical();
+
+ //     if (y == 10||y == 20)
+ //     {
+ //       printf("\n");
+ //       bigWhiteSpace();
+ //     }
+
+ //     y--;
+
+ //   }
+ //   printf("\xB3\n");
+ //   i--;
+ // }
+	//bigWhiteSpace();
+ // while (j > 0)
+ // {
+ //   printFieldHorizontal();
+ //   j--;
+ // }
+ // printf("\xC5\n");
 }
 
+void showHighscore()
+{
+	lineBreaks();
+	bigWhiteSpace();
+	printf("Bestenliste\n");
+}
 /******************************************************************************
 Name: main
 Parameter: /
@@ -347,7 +381,7 @@ Description:
 ******************************************************************************/
 int main()
 {
-  printField();
+	showHighscore();
 
   system("pause");
   return 0;
