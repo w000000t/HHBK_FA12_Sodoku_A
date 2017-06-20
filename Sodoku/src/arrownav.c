d5340379c37dba3fb2752d641cede3cf79a2e372
w000000t <code@simon-marx.de>
2017-06-20 11:03:20 +0200

    Merge with master (#5)
    * Merge with Nick bedienfeld (#1)
    
    * Navigation via ArrowKeys
    
    Overrides the chararray(keypressed) with up, down, left, right, return, escape, enter in capital letters.
    
    * Add files via upload
    
    * Navigation via ArrowKeys
    
    This Class takes the char "keypressed" and ovverides it with the content UP, DOWN, LEFT, RIGHT, RETURN, ESCAPE, ENTER in caps.
    
    * Navigation via Arrowkeys
    
    This class takes the char "keypressed" and overrides it with UP, DOWN, LEFT, RIGHT, RETURN, ESCAPE, ENTER in caps.
    
    * Update arrownav.c
    
    * Navigation via Arrowkeys
    
    Overrides the chararray(keypressed) with up, down, left, right, return, escape, enter in capital letters.
    
    * Merge with Timo lukas logik userinterface (#2)
    
    * Upload Logic Multiple Menus
    
    This contains most of the logic for the 3 menus "main menu", "logged in main menu" and "difficulty"
    
    * logic for multiple site functions (not done)
    
    * logic for multiple site functions (not done)
    
    * Merge with Yannik userinterface (#3)
    
    * Dateien hochzuladen
    
    * Added comments
    
    * Added comments
    
    * Delete Benutzeroberflächen.sln

---
+++ Sodoku/src/arrownav.c
@@ -0,0 +1,35 @@
+#include<conio.h> //Header file for the kbhit command
+#include<stdio.h>
+#include<stdlib.h> 
+
+
+void navigation(char keypressed[])
+{
+	int ch;
+	int loop = 0;
+
+	while (loop != 1) //Looping while none of the important keys is pressed
+	{
+		if (kbhit()) //Waiting for pressing a Key
+		{
+			ch=getch();
+			if (ch==224 || ch==0) //Filtering 0 and 224, 
+								  //because they cause problems
+			{
+				ch=getch(); //Second getch to get the right key
+			}
+			switch (ch) //Giving "keypressed" the right content
+			{
+			case 72: keypressed="UP"; loop=1; break; //Setting "loop" on to 1
+			case 80: keypressed="DOWN"; loop=1; break;//to brake the while loop
+			case 75: keypressed="LEFT"; loop=1; break;
+			case 77: keypressed="RIGHT"; loop=1; break;
+			case 8: keypressed="RETURN"; loop=1; break;
+			case 27: keypressed="ESCAPE"; loop=1; break;
+			case 13: keypressed="ENTER"; loop=1; break;
+			default:
+				break;
+			}
+		}
+	}
+}
