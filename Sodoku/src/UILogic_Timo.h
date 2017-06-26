dc509907cc84dcc51ee3850a4219a70ff044614d
LumesseTimo2 <icherster@web.de>
2017-06-26 08:05:50 +0200

    Datei-Header

--- Sodoku/src/UILogic_Timo.h
+++ Sodoku/src/UILogic_Timo.h
@@ -1,5 +1,16 @@
+/*
+================================================================================
+Autoren: Timo Scheile
+Klasse: FA12
+Programmname: UILogic_Timo.h
+Datum: 23.0
+Beschreibung: Deklaration von Funktionen, die mit der UI-Logik zu tun haben.
+Version: 1.0
+Compiler: Visual Studio
+===============================================================================
+*/
+
 void readUsername(char cUsername[]);
 void readPassword(char cPassword[]);
 void Login();
 void Register();
-void getHighscore(int user_id);
